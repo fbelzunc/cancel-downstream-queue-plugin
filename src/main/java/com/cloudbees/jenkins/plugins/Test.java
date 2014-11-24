0380b231d038059f1d3cc68882e40bc13d104138
Felix Belzunce Arcos <fbelzuncearcos@cloudbees.com>
2014-11-24 22:07:13 +0100

    cancel build plus queue tasks

--- src/main/java/com/cloudbees/jenkins/plugins/Test.java
+++ src/main/java/com/cloudbees/jenkins/plugins/Test.java
@@ -1,20 +1,18 @@
 package com.cloudbees.jenkins.plugins;
 
 import hudson.Extension;
-import hudson.model.AbstractProject;
-import hudson.model.Items;
-import hudson.model.Job;
+import hudson.model.*;
 import hudson.model.Queue;
 import hudson.model.queue.QueueListener;
 import jenkins.model.Jenkins;
 
 import javax.naming.Context;
+import java.io.IOException;
 import java.util.*;
+import java.util.concurrent.Executor;
 
 @Extension
 public class Test extends QueueListener {
-
-    private final Map<Queue.WaitingItem, Context> waiting = new WeakHashMap<Queue.WaitingItem, Context>();
 
     public static Test instance() {
         return Jenkins.getInstance().getExtensionList(QueueListener.class).get(Test.class);
@@ -23,27 +21,52 @@ public class Test extends QueueListener {
     @Override
     public void onEnterWaiting(Queue.WaitingItem wi) {
 
+
         String jobName = wi.task.getName();
 
-        if (jobName.equals("job-1")) {
+        Job job = (Job) Jenkins.getInstance().getItemByFullName(jobName);
 
-            Job jobTriggered = (Job) Jenkins.getInstance().getItemByFullName(jobName);
-            AbstractProject jobTriggered2 = (AbstractProject) jobTriggered;
-            List<AbstractProject> childJobs = jobTriggered2.getDownstreamProjects();
-            Iterator childJobsIterator = childJobs.iterator();
+        AbstractProject project = (AbstractProject) job;
 
-            //TODO need to check timestamp of first job in case it was already built
-            if (jobTriggered2.getTrigger(CancelDownstreamQueueTrigger.class) != null)
-                while (childJobsIterator.hasNext()) {
-                    AbstractProject child = (AbstractProject) childJobsIterator.next();
+        if (project.getTrigger(CancelDownstreamQueueTrigger.class)!=null) {
+            cancelDownstreamJobs(job);
 
-                    Queue.Item[] childs = Jenkins.getInstance().getQueue().getItems();
-                        for(int i=-0; i<childs.length; i++) {
-                            Queue.Item myItem = childs[i];
-                            System.out.println("Cancelling " + myItem.task.getDisplayName());
-                            Jenkins.getInstance().getQueue().cancel(myItem.task);
-                        }
-                }
         }
     }
+
+    public void cancelDownstreamJobs(Job job) {
+
+        AbstractProject project = (AbstractProject) job;
+        //Get current date time with Calendar()
+        Calendar cal = Calendar.getInstance();
+        Date currentDate = cal.getTime();
+        long currentTimeLong = currentDate.getTime();
+
+        //Check if the job is not already building in an Executors
+        //If it is already building -> Cancel the build
+        if (job.isBuilding()) {
+            Date date = job.getLastBuild().getTime();
+            long buildDate = date.getTime();
+            long diff= currentTimeLong - buildDate;
+            if((diff)< 30*1000)
+                job.getLastBuild().getExecutor().doStop();
+        }
+
+        //Next step is to cancel the downstream jobs
+        List<AbstractProject> childProjects = project.getDownstreamProjects();
+        Iterator childProjectsIterator = childProjects.iterator();
+
+        while (childProjectsIterator.hasNext()) {
+            AbstractProject childProject = (AbstractProject) childProjectsIterator.next();
+            if (childProject.isBuilding())
+                childProject.getLastBuild().getExecutor().doStop();
+            else if (childProject.isInQueue()) {
+                Queue.Item myItem = childProject.getQueueItem();
+                Jenkins.getInstance().getQueue().cancel(myItem.task);
+            }
+
+        }
+    }
+
+
 }
